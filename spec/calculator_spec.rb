052cedd6bd9df749f69d3837cbabeca21f5efb98
Eugen Mueller <mueller@crowddesk.de>
2021-10-04 22:23:11 +0200

    Add error specs.

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -2,15 +2,14 @@
 
 RSpec.describe InterestDays::Calculator do
   let(:start_date) { Date.today }
-  let(:end_date) { Date.today + 118 }
-
+  let(:end_date) { Date.today + 360 }
   let(:calculator) { described_class.new(start_date: start_date, end_date: end_date, strategy: strategy) }
 
   describe "#interest_day_count_factor" do
     let(:strategy) { :isda_act_360 }
 
     it { expect(calculator).to respond_to(:interest_day_count_factor) }
-    it { expect(calculator.interest_day_count_factor).to be_instance_of(Float) }
+    it { expect(calculator.interest_day_count_factor).to be(1.0) }
   end
 
   describe "isda act 360 strategy based calculation" do
@@ -29,5 +28,12 @@ RSpec.describe InterestDays::Calculator do
     let(:strategy) { :isda_30_e_360 }
 
     it { expect(calculator.strategy).to be(InterestDays::Calculation::Isda30e360) }
+  end
+
+  describe "when strategy not available" do
+    let(:strategy) { :foo }
+
+    it { expect(calculator.strategy).to be_nil }
+    it { expect { calculator.interest_day_count_factor }.to raise_error }
   end
 end
