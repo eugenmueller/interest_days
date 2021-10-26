b9d21f91125220596d37ca7b6cf5e8edcb7a536e
Eugen Mueller <mueller@crowddesk.de>
2021-10-26 10:47:33 +0200

    Add spec for act 364 strategy.

---
+++ spec/calculation/isda_act_364_spec.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+RSpec.describe InterestDays::Calculation::IsdaAct360 do
+  describe "#calculate" do
+    let(:start_date) { Date.today }
+    let(:end_date) { Date.today + 78 }
+
+    subject(:calc) { described_class.new(start_date: start_date, end_date: end_date) }
+
+    it "calculate the right day count factor" do
+      expectation = (end_date - start_date).fdiv(360)
+    end
+  end
+end
