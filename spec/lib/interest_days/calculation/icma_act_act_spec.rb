d3597db45202b2688801841396b180b6bbf6be34
Eugen Müller <eugen.mllr@gmail.com>
2022-11-11 18:36:55 +0100

    Fix rubocop complains.

--- spec/lib/interest_days/calculation/icma_act_act_spec.rb
+++ spec/lib/interest_days/calculation/icma_act_act_spec.rb
@@ -1,151 +1,83 @@
 # frozen_string_literal: true
 
 RSpec.describe InterestDays::Calculation::IcmaActAct do # rubocop:disable Metrics/BlockLength
-  describe "#day_count_factor" do # rubocop:disable Metrics/BlockLength
-    subject(:calc) { described_class.new(start_date: start_date, end_date: end_date) }
+  subject(:calc) { described_class.new(start_date: start_date, end_date: end_date) }
 
-    context "when start date in leap year" do
-      let(:end_date) { Date.new(2021, 2, 3) }
+  describe "when start date in leap year" do
+    let(:end_date) { Date.new(2021, 2, 3) }
 
-      context "when 29th of february not in period" do
-        let(:start_date) { Date.new(2020, 5, 3) }
+    context "when 29th of february not in period" do
+      let(:start_date) { Date.new(2020, 5, 3) }
 
-        it "calculates the right day count factor" do
-          expectation = (end_date - start_date).fdiv(described_class::DAYS_IN_YEAR)
-
-          expect(calc.day_count_factor).to eql(expectation)
-        end
-      end
-
-      context "when 29th of february is in period" do
-        let(:start_date) { Date.new(2020, 2, 3) }
-
-        it "calculates the right day count factor" do
-          expectation = (end_date - start_date).fdiv(described_class::DAYS_IN_LEAP_YEAR)
-
-          expect(calc.day_count_factor).to eql(expectation)
-        end
+      it "calculates the right day count factor" do
+        expect(calc.day_count_factor).to eql((end_date - start_date).fdiv(described_class::DAYS_IN_YEAR))
       end
     end
 
-    context "when end_date in leap year" do
-      let(:start_date) { Date.new(2019, 6, 1) }
+    context "when 29th of february is in period" do
+      let(:start_date) { Date.new(2020, 2, 3) }
 
-      context "when 29th of february not in period" do
-        let(:end_date) { Date.new(2020, 2, 3) }
-
-        it "calculates the right day count factor" do
-          expectation = (end_date - start_date).fdiv(described_class::DAYS_IN_YEAR)
-
-          expect(calc.day_count_factor).to eql(expectation)
-        end
+      it "calculates the right day count factor" do
+        expect(calc.day_count_factor).to eql((end_date - start_date).fdiv(described_class::DAYS_IN_LEAP_YEAR))
       end
+    end
+  end
 
-      context "when 29th of february is in period" do
-        let(:end_date) { Date.new(2020, 5, 3) }
+  describe "when end_date in leap year" do
+    let(:start_date) { Date.new(2019, 6, 1) }
 
-        it "calculates the right day count factor" do
-          expectation = (end_date - start_date).fdiv(described_class::DAYS_IN_LEAP_YEAR)
+    context "when 29th of february not in period" do
+      let(:end_date) { Date.new(2020, 2, 3) }
 
-          expect(calc.day_count_factor).to eql(expectation)
-        end
+      it "calculates the right day count factor" do
+        expect(calc.day_count_factor).to eql((end_date - start_date).fdiv(described_class::DAYS_IN_YEAR))
       end
     end
 
-    context "when start_date and end_date in same year" do
-      context "when year is a leap year" do
-        context "when 29th of feburary is period" do
-          let(:start_date) { Date.new(2020, 1, 1) }
-          let(:end_date) { Date.new(2020, 12, 31) }
+    context "when 29th of february is in period" do
+      let(:end_date) { Date.new(2020, 5, 3) }
 
-          it "calculates the right day count factor" do
-            expectation = (end_date - start_date).fdiv(described_class::DAYS_IN_LEAP_YEAR)
-
-            expect(calc.day_count_factor).to eql(expectation)
-          end
-        end
-
-        context "when 29th of feburary is not in period" do
-          let(:start_date) { Date.new(2020, 3, 1) }
-          let(:end_date) { Date.new(2020, 12, 31) }
-
-          it "calculates the right day count factor" do
-            expectation = (end_date - start_date).fdiv(described_class::DAYS_IN_YEAR)
-
-            expect(calc.day_count_factor).to eql(expectation)
-          end
-        end
+      it "calculates the right day count factor" do
+        expect(calc.day_count_factor).to eql((end_date - start_date).fdiv(described_class::DAYS_IN_LEAP_YEAR))
       end
     end
+  end
 
-    context "when start_date and end_date span over multiple years" do
+  describe "when start_date and end_date in same year" do
+    let(:end_date) { Date.new(2020, 12, 31) }
+
+    context "when 29th of feburary is period" do
       let(:start_date) { Date.new(2020, 1, 1) }
-      let(:end_date) { Date.new(2026, 12, 30) }
 
-      it "calculates the right factor" do
-        start_date_period_factor =
-          (described_class::DAYS_IN_LEAP_YEAR - start_date.yday).fdiv(described_class::DAYS_IN_LEAP_YEAR)
-        end_date_period_factor = end_date.yday.fdiv(described_class::DAYS_IN_YEAR)
-        years_factor = (end_date.year - start_date.year) - 1
-
-        expectation = start_date_period_factor + years_factor + end_date_period_factor
-
-        expect(calc.day_count_factor).to eql(expectation)
+      it "calculates the right day count factor" do
+        expect(calc.day_count_factor).to eql((end_date - start_date).fdiv(described_class::DAYS_IN_LEAP_YEAR))
       end
     end
 
-    describe "integration example normal year" do
-      let(:start_date) { Date.new(1999, 2, 1) }
-      let(:end_date) { Date.new(1999, 7, 1) }
+    context "when 29th of feburary is not in period" do
+      let(:start_date) { Date.new(2020, 3, 1) }
 
-      let(:days) { (end_date - start_date).to_i }
-      let(:amount) { 10_000.00 }
-
-      let(:interest) { 10.00 }
-
-      it "calculates the interest with the right factor" do
-        expectation = amount / 100 * interest * 150.fdiv(described_class::DAYS_IN_YEAR)
-
-        expect(amount / 100 * interest * calc.day_count_factor).to eql(expectation)
+      it "calculates the right day count factor" do
+        expect(calc.day_count_factor).to eql((end_date - start_date).fdiv(described_class::DAYS_IN_YEAR))
       end
     end
+  end
 
-    describe "integration example leap year" do
-      let(:start_date) { Date.new(1999, 7, 1) }
-      let(:end_date) { Date.new(2000, 7, 1) }
+  describe "when start_date and end_date span over multiple years" do
+    let(:start_date) { Date.new(2020, 1, 1) }
+    let(:end_date) { Date.new(2026, 12, 30) }
 
-      let(:days) { (end_date - start_date).to_i }
-      let(:amount) { 10_000.00 }
+    let(:expectation) do
+      start_date_period_factor =
+        (described_class::DAYS_IN_LEAP_YEAR - start_date.yday).fdiv(described_class::DAYS_IN_LEAP_YEAR)
+      end_date_period_factor = end_date.yday.fdiv(described_class::DAYS_IN_YEAR)
+      years_factor = (end_date.year - start_date.year) - 1
 
-      let(:interest) { 10.00 }
-
-      it "calculates the interest with the right factor" do
-        expectation = amount / 100 * interest * (end_date - start_date).fdiv(described_class::DAYS_IN_LEAP_YEAR)
-
-        expect(amount / 100 * interest * calc.day_count_factor).to eql(expectation)
-      end
+      start_date_period_factor + years_factor + end_date_period_factor
     end
 
-    describe "integration example multiple years" do
-      let(:start_date) { Date.new(2000, 2, 1) }
-      let(:end_date) { Date.new(2005, 7, 1) }
-
-      let(:days) { (end_date - start_date).to_i }
-      let(:amount) { 10_000.00 }
-
-      let(:interest) { 10.00 }
-
-      it "calculates the interest with the right factor" do
-        start_date_period_factor =
-          (described_class::DAYS_IN_LEAP_YEAR - start_date.yday).fdiv(described_class::DAYS_IN_LEAP_YEAR)
-        end_date_period_factor = end_date.yday.fdiv(described_class::DAYS_IN_YEAR)
-        years_factor = (end_date.year - start_date.year) - 1
-
-        day_count_factor = start_date_period_factor + years_factor + end_date_period_factor
-        expectation = amount / 100 * interest * day_count_factor
-
-        expect(amount / 100 * interest * calc.day_count_factor).to eql(expectation)
-      end
+    it "calculates the right factor" do
+      expect(calc.day_count_factor).to eql(expectation)
     end
   end
 end
--- spec/lib/interest_days/calculation/isda_act_act_spec.rb
+++ spec/lib/interest_days/calculation/isda_act_act_spec.rb
@@ -3,10 +3,10 @@
 RSpec.describe InterestDays::Calculation::IsdaActAct do # rubocop:disable Metrics/BlockLength
   describe "#day_count_factor" do # rubocop:disable Metrics/BlockLength
     context "when start date in leap year" do
+      subject(:calc) { described_class.new(start_date: start_date, end_date: end_date) }
+
       let(:start_date) { Date.new(2020, 5, 3) }
       let(:end_date) { Date.new(2021, 2, 3) }
-
-      subject(:calc) { described_class.new(start_date: start_date, end_date: end_date) }
 
       it "calculates the right day count factor" do
         year_fraction_end = end_date.yday.fdiv(365)
@@ -18,10 +18,10 @@ RSpec.describe InterestDays::Calculation::IsdaActAct do # rubocop:disable Metric
     end
 
     context "when end date in leap year" do
+      subject(:calc) { described_class.new(start_date: start_date, end_date: end_date) }
+
       let(:start_date) { Date.new(2019, 5, 3) }
       let(:end_date) { Date.new(2020, 2, 3) }
-
-      subject(:calc) { described_class.new(start_date: start_date, end_date: end_date) }
 
       it "calculates the right day count factor" do
         year_fraction_end = end_date.yday.fdiv(366)
@@ -33,10 +33,10 @@ RSpec.describe InterestDays::Calculation::IsdaActAct do # rubocop:disable Metric
     end
 
     context "when end and start date not in leap year" do
+      subject(:calc) { described_class.new(start_date: start_date, end_date: end_date) }
+
       let(:start_date) { Date.new(2019, 1, 3) }
       let(:end_date) { Date.new(2019, 7, 3) }
-
-      subject(:calc) { described_class.new(start_date: start_date, end_date: end_date) }
 
       it "calculates the right day count factor" do
         expectation = (end_date - start_date).fdiv(365)
@@ -46,10 +46,10 @@ RSpec.describe InterestDays::Calculation::IsdaActAct do # rubocop:disable Metric
     end
 
     context "when end and start date in leap year" do
+      subject(:calc) { described_class.new(start_date: start_date, end_date: end_date) }
+
       let(:start_date) { Date.new(2020, 1, 3) }
       let(:end_date) { Date.new(2020, 7, 3) }
-
-      subject(:calc) { described_class.new(start_date: start_date, end_date: end_date) }
 
       it "calculates the right day count factor" do
         expectation = (end_date - start_date).fdiv(366)
@@ -59,10 +59,10 @@ RSpec.describe InterestDays::Calculation::IsdaActAct do # rubocop:disable Metric
     end
 
     context "when leap year between start and end date" do
+      subject(:calc) { described_class.new(start_date: start_date, end_date: end_date) }
+
       let(:start_date) { Date.new(2019, 1, 3) }
       let(:end_date) { Date.new(2021, 7, 3) }
-
-      subject(:calc) { described_class.new(start_date: start_date, end_date: end_date) }
 
       it "calculates the right day count factor" do
         year_fraction_end = end_date.yday.fdiv(365)
@@ -76,29 +76,29 @@ RSpec.describe InterestDays::Calculation::IsdaActAct do # rubocop:disable Metric
     end
 
     context "when start date after end date" do
+      subject(:calc) { described_class.new(start_date: start_date, end_date: end_date) }
+
       let(:start_date) { Date.new(2020, 1, 3) }
       let(:end_date) { Date.new(2019, 7, 3) }
-
-      subject(:calc) { described_class.new(start_date: start_date, end_date: end_date) }
 
       it { expect { calc.day_count_factor }.to raise_error(InterestDays::Calculation::StartDateBeforeEndDateError) }
     end
   end
 
   describe "#start_date_after_end_date?" do
+    subject(:calc) { described_class.new(start_date: start_date, end_date: end_date) }
+
     let(:start_date) { Date.new(2020, 5, 3) }
     let(:end_date) { Date.new(2021, 2, 3) }
-
-    subject(:calc) { described_class.new(start_date: start_date, end_date: end_date) }
 
     it { expect(calc.send(:start_date_after_end_date?)).to be false }
   end
 
   describe "#days" do
+    subject(:calc) { described_class.new(start_date: start_date, end_date: end_date) }
+
     let(:start_date) { Date.new(2020, 5, 3) }
     let(:end_date) { Date.new(2021, 2, 3) }
-
-    subject(:calc) { described_class.new(start_date: start_date, end_date: end_date) }
 
     it { expect(calc.send(:days)).to eq(end_date - start_date) }
   end
